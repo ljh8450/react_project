659bd253a8f024ed82011894d11014fc3ef10b6b
Miniberry <dzigh30@gmail.com>
2024-10-01 21:08:30 +0900

    first assignment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
 import React from 'react'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Blog</a>
+          {/* <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button> */}
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="navbar-nav">
+              <ul class="nav justify-content-end">
+                <li class="nav-item">
+                  <a class="nav-link" href="#">MY ACCOUNT</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">MY PAGE</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">SIGN IN</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">SIGN UP</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
     </div>
   );
 }
